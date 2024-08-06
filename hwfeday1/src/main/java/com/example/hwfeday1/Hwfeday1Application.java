6d1f93ba7f98e37de3f87680adcdf69b32cafbaf
Seungminaa <87366067+Seungminaa@users.noreply.github.com>
2024-08-06 15:02:19 +0900

    0806

---
+++ hwfeday1/src/main/java/com/example/hwfeday1/Hwfeday1Application.java
@@ -0,0 +1,13 @@
+package com.example.hwfeday1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hwfeday1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hwfeday1Application.class, args);
+	}
+
+}
