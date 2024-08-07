8448928011ae0539de17b72e7b03e552c54a3ffe
Seungminaa <87366067+Seungminaa@users.noreply.github.com>
2024-08-07 16:25:54 +0900

    0807

---
+++ tempexam/src/main/java/com/example/tempexam/TempexamApplication.java
@@ -0,0 +1,13 @@
+package com.example.tempexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempexamApplication.class, args);
+	}
+
+}
