b3d048e6a5b924f7d60980c5d3b360a63ec84408
dmg04144 <dmg04144@naver.com>
2024-08-08 16:56:33 +0900

    0808

---
+++ loginexam/src/main/java/com/example/loginexam/LoginexamApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginexamApplication.class, args);
+	}
+
+}
---
+++ loginexam/src/test/java/com/example/loginexam/LoginexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
